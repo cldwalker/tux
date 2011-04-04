e96d961bd2fe9828b0198f3f3a9b8aa3b29b4a23
Gabriel Horner <gabriel.horner@gmail.com>
2011-04-04 06:15:59 -0400

    format commands

--- lib/tux/commands.rb
+++ lib/tux/commands.rb
@@ -2,17 +2,19 @@ require 'ripl'
 
 module Tux
   module Commands
+    SETTINGS = %w{methodoverride inline_templates}
+
     def routes
       Tux.app_class.routes.inject([]) {|arr, (k,v)|
         arr += v.map {|e|
-          [ k, (str = e[0].inspect[%r{/\^(.*)\$/}, 1]) ? str.tr('\\', '') : e[0] ]
+          [k, (str = e[0].inspect[%r{/\^(.*)\$/}, 1]) ? str.tr('\\', '') : e[0]]
         }
       }
     end
 
     def settings
-      meths = (class << Tux.app_class; self; end).instance_methods(false).sort.map(&:to_s).
-        select {|e| e[/=$/] }.map {|e| e[0..-2] } - %w{methodoverride inline_templates}
+      meths = (class << Tux.app_class; self; end).instance_methods(false).
+        sort.map(&:to_s).select {|e| e[/=$/] }.map {|e| e[0..-2] } - SETTINGS
       meths.map {|meth| [meth, Tux.app_class.send(meth)] }
     end
 
@@ -25,5 +27,22 @@ module Tux
       end
     end
   end
+
+  module CommandsFormatted
+    def self.format(arr)
+      arr = arr.map {|e| [e[0], e[1].inspect] }
+      max1 = arr.map {|e| e[0].length }.max
+      max2 = arr.map {|e| e[1].length }.max
+      arr.map {|k,v| "%-*s  %-*s" % [max1, k, max2, v] }
+    end
+
+    def routes
+      puts CommandsFormatted.format(super)
+    end
+
+    def settings
+      puts CommandsFormatted.format(super)
+    end
+  end
 end
-Ripl::Commands.include Tux::Commands
+Ripl::Commands.include Tux::CommandsFormatted, Tux::Commands
