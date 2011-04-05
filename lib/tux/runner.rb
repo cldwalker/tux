4726a8209e6f00ed7d8bd0be5a6a4781f7ebedc4
Gabriel Horner <gabriel.horner@gmail.com>
2011-04-05 02:01:03 -0400

    add -c option to specify rack config file

--- lib/tux/runner.rb
+++ lib/tux/runner.rb
@@ -1,5 +1,14 @@
 module Tux
   class Runner < Ripl::Runner
     self.app = 'tux'
+    add_options ['-c, --config FILE', 'Set rack config file i.e. config.ru']
+
+    def self.parse_option(option, argv)
+      if option[/(?:-c|--config)=?(.*)/]
+        ENV['RACK_CONFIG'] = $1.empty? ? argv.shift.to_s : $1
+      else
+        super
+      end
+    end
   end
 end
